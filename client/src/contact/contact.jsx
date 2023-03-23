72fe2ae81fdb9e6d436a47fdbc7317e495279fe6
ronnakash <ronnakash@gmail.com>
2023-03-23 18:56:39 +0200

    contact details refactor

--- client/src/contact/contact.jsx
+++ client/src/contact/contact.jsx
@@ -18,23 +18,23 @@ const Details = ({ name, location, company, address, phone }) => {
   return (
     <div className="details">
       <div className="details-row">
-        <span className="details-label">Name:</span>
+        {/* <span className="details-label">Name:</span> */}
         <span className="details-value">{name}</span>
       </div>
       <div className="details-row">
-        <span className="details-label">Location:</span>
+        {/* <span className="details-label">Location:</span> */}
         <span className="details-value">{location}</span>
       </div>
       <div className="details-row">
-        <span className="details-label">Company:</span>
+        {/* <span className="details-label">Company:</span> */}
         <span className="details-value">{company}</span>
       </div>
       <div className="details-row">
-        <span className="details-label">Address:</span>
+        {/* <span className="details-label">Address:</span> */}
         <span className="details-value">{address}</span>
       </div>
       <div className="details-row">
-        <span className="details-label">Phone:</span>
+        {/* <span className="details-label">Phone:</span> */}
         <span className="details-value">{phone}</span>
       </div>
     </div>
