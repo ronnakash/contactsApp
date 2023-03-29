1e5a90420f2e98f2f28c1cf1793dd3e978fd7a9d
ronnakash <ronnakash@gmail.com>
2023-03-29 22:59:06 +0300

    cors options

--- server/src/server.js
+++ server/src/server.js
@@ -14,14 +14,16 @@ app.use(cors({
   methods: ['PUT', 'POST', 'PATCH', 'DELETE', 'GET']
 }));
 
-// set CORS headers for preflight requests from browsers
-app.options('*', function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+// // set CORS headers for preflight requests from browsers
+// app.options('*', function(req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// });
+
+app.options('*', cors());
 
 // GET /contacts - get all contacts
 app.get('/contacts', api.get);
