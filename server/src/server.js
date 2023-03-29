9f61b310c6d4d4b5f404cc4207d4d9edd7075699
ronnakash <ronnakash@gmail.com>
2023-03-29 22:57:12 +0300

    cors options

--- server/src/server.js
+++ server/src/server.js
@@ -15,7 +15,7 @@ app.use(cors({
 }));
 
 // set CORS headers for preflight requests from browsers
-app.get('*', function(req, res, next) {
+app.options('*', function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
